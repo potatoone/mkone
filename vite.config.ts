a842980ea3680ce48034511156c49a18818e77f6
potatoone <2428100619@qq.com>
2026-01-21 17:44:34 +0800

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite';
 
-// 要保留的文件/目录
-const preserveFiles = [
-  { src: 'docs', dest: 'docs' }    // 保留 docs 目录（按需添加）
-];
-
 export default defineConfig({
-  base: './', // 相对路径
+  base: './wiki', // 相对路径
   build: {
-    outDir: 'demo', // 打包目录
+    outDir: 'wiki', // 打包目录
   }
 });
